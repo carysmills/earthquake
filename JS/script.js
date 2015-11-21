ba15bc6cfe5bb02576952851f985bde0b835c707
Carys Mills <mills.carys@gmail.com>
2015-11-21 18:54:36 -0500

    adding attempt with chart.js that doesn't quite work

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,12 @@
+// declare the variables
+
 var quakes = {};
 quakes.mag = [];
+quakes.felt = [];
+quakes.where =[];
+quakes.time = [];
+quakes.deets = [];
+quakes.date = [];
 
 //*Get quake data
 quakes.getData = function() {
@@ -15,24 +22,52 @@ quakes.getData = function() {
 
 //sort quake data
 quakes.sortData = function(data) {
-	quakes.number = data.metadata.request.resultCount;
 	  for (var info in data) {
 	  	if (data[info].request) {
 		  	quakes.number = data[info].request.resultCount;
 	  	} else {
-	  		quakes.details = data[info];
-	  		quakes.mag = quakes.details.magnitude;
-	  		quakes.felt = quakes.details.felt;
-	  		quakes.where = quakes.details.geoJSON.coordinates;
-	  		quakes.time = quakes.details.origin_time;
+	  		quakes.mag.push(data[info].magnitude);
+	  		quakes.string = quakes.mag.toString().replace(/,/g, ', ');
+	  		quakes.felt.push(data[info].felt);
+	  		quakes.where.push(data[info].geoJSON.coordinates);
+	  		quakes.time.push(data[info].origin_time);
+	  		quakes.date.push(data[info].origin_time.replace(/T.*$/, ""));
+	  		quakes.deets.push(data[info].location.en);
 	  	}
 	  }
-};	
+};
 
 //put number of earthquakes in the last year on the page
 quakes.printData = function(){
 	$(".number").text(quakes.number);
 };
+
+
+//start of graph
+            // line chart data
+            var cheeseData = {
+                labels : [quakes.date],
+                datasets : [
+                  {
+            label: "Specialty cheese consuption per capita",
+            fillColor: "#FFF380",
+            strokeColor: "#800080",
+            pointColor: "#FFF380",
+            pointStrokeColor: "#800080",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "#800080",
+                    data : [quakes.string]
+                  }
+    ]
+};
+
+            // get line chart canvas
+            var cheese = document.getElementById('cheese').getContext('2d');
+            // draw line chart
+            new Chart(cheese).Line(cheeseData, {
+        });
+
+ //end of graph           
 
 
 //put it all together in the init 
